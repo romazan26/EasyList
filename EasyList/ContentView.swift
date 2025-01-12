e79c050cc14f61e71beca09ea83da8907fa6a680
romazan26 <130604263+romazan26@users.noreply.github.com>
2025-01-12 23:28:31 +0300

    Initial Commit

---
+++ EasyList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyList
+//
+//  Created by Роман on 12.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EasyList/EasyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyListApp.swift
+//  EasyList
+//
+//  Created by Роман on 12.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct EasyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
